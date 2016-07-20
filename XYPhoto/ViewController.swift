0bc3748584295328619a2bb9491c7bb00fc2f778
Janna111 <448745259@qq.com>
2016-07-20 11:55:47 +0800

    Initial Commit

---
+++ XYPhoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XYPhoto
+//
+//  Created by jiaxiaoyan on 16/7/20.
+//  Copyright © 2016年 jiaxiaoyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
